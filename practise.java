0252489b3ce6cb903a33c9c96207f5ed067539e0
Devansh Singla <devanshsingla@192.168.1.14>
2022-03-30 16:08:46 +0530

    first commit

---
+++ practise.java
@@ -0,0 +1,119 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class practise{
+    public static void main(String[] args){
+        // question1
+        // int a=27;
+        // int b=32;
+        // int c=78;
+        // int sum=a+b+c;
+        // System.out.println(sum);
+
+        // Question2 find cgpa
+        // float sub1 = 96;
+        // float sub2 = 89;
+        // float sub3 = 45;
+        // float percentage = ((sub1+sub2+sub3)/300*100)/10;
+        // System.out.println(percentage);
+
+        // Question3 input name and print greetings.
+        // System.out.print("Please Enter Your Name - ");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println("Hey "+name+", How's You ??");
+
+        // Question4 Convert kilometers to miles.
+        // System.out.print("Enter value in Kilometers : ");
+        // Scanner sc = new Scanner(System.in);
+        // float kilometers = sc.nextFloat();
+        // double miles = kilometers*0.621371;
+        // System.out.println("Value in Miles is :"+miles);
+        
+
+        // Question5 Add grade + 8 and then grade - 7.
+        // char grade = 'A';
+        // System.out.println(grade);
+        // grade = (char)(grade + 8);
+        // System.out.println(grade);
+        // grade = (char)(grade - 7);
+        // System.out.println(grade);
+
+        // Question6 Use compairision operators to check wheather a user entered number is smaller than a number or not.
+        // int x = 68;
+        // System.out.print("Enter the number : ");
+        // Scanner sc = new Scanner(System.in);
+        // int number = sc.nextInt();
+        // if(number<x){
+        //     System.out.println("Your number is smaller !");
+        // }
+        // else if(number==x){
+        //     System.out.println("both numbers are equal.");
+        // }
+        // else{
+        //     System.out.println("Your number is greater.");
+        // }
+
+        // Question7 Making a Calculator in java.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the first number :");
+        // float no1 = sc.nextFloat();
+        // System.out.print("Enter the second number :");
+        // float no2 = sc.nextFloat();
+        // System.out.println("Press 1 for addition ");
+        // System.out.println("Press 2 for subtraction ");
+        // System.out.println("Press 3 for multiplication ");
+        // System.out.println("Press 4 for division ");
+        // int operation = sc.nextInt();
+        // if(operation==1){
+        //     System.out.print("The sum is :");
+        //     System.out.println(no1+no2);
+        // }
+        // else if(operation==2){
+        //     System.out.print("The result after subtraction is :");
+        //     System.out.println(no1-no2);
+        // }
+        // else if(operation==3){
+        //     System.out.print("The product is :");
+        //     System.out.println(no1*no2);
+        // }
+        // else if(operation==4){
+        //     System.out.print("The division result is :");
+        //     System.out.println(no1/no2);
+        // }
+        // else{
+        //     System.out.println("Wrong Input Entered !");
+        // }
+        
+        // Question8 Factorial of a number i.e n.
+        // System.out.print("Enter the number till you want factorial(n) :");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int i,fact=1;
+        // for(i=1;i<=n;i++){
+        //     fact=fact*i;
+        // }
+        // System.out.println(fact);
+        
+        // Question9 Febbonacci series upto n number.
+        // System.out.print("Enter the number upto which febbonacci is reqd.(n) : ");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int x=0;
+        // int y=1;
+        // int z=0;
+        // int i=0;
+        // System.out.print(x+"   "+y+"   ");
+        // while(i<n-2){
+        //     z=x+y;
+        //     System.out.print(z+"   ");
+        //     x=y;
+        //     y=z;
+        //     ++i;
+        // }
+
+
+
+
+    }
+}
